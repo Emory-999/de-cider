4cb7804d5a1e1975a31b33ac637fbcd97817887f
Emory-999 <emoryandrewmiller@gmail.com>
2025-09-29 13:47:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function agerating(){
+    var feedback = document.getElementById("feedback").value;
+var age1= document.getElementById("Salary").value;
+feedback=age1
+}
+
+document.getElementById("salary").innerHTML = feedback
